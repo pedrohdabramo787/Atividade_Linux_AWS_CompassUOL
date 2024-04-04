3cfe0d294f6e50affcc3bd0d59f4fefdba7c6336
Pedro Haddad Dabramo <148357632+pedrohdabramo787@users.noreply.github.com>
2024-04-04 09:45:44 -0300

    Update script_5_mins_binbash.sh

--- script_5_mins_binbash.sh
+++ script_5_mins_binbash.sh
@@ -4,7 +4,7 @@ service_name="Apache"
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
 if systemctl is-active httpd; then
-    echo "Data/Hora: $timestamp - $service_name - Serviço HTTPD - Status: O serviço está online" >> ~/efs-mount-point/httpd-online.txt
+    echo "Data/Hora: $timestamp - $service_name - Serviço HTTPD - Status: O serviço está online" >> /mnt/efs/httpd-online.txt
 else
-    echo "Data/Hora: $timestamp - $service_name - Serviço HTTPD - Status: O serviço está offline" >> ~/efs-mount-point/httpd-offline.txt
+    echo "Data/Hora: $timestamp - $service_name - Serviço HTTPD - Status: O serviço está offline" >> /mnt/efs/httpd-offline.txt
 fi
